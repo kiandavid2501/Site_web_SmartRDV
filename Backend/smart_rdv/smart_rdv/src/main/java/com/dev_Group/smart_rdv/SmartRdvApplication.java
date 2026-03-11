b18c31e614a88e71990376e8b50833cd90b0b6a5
David <kiandavid2501@gmail.com>
2026-03-11 22:51:28 +0000

    mise en place du projets

---
+++ Backend/smart_rdv/smart_rdv/src/main/java/com/dev_Group/smart_rdv/SmartRdvApplication.java
@@ -0,0 +1,13 @@
+package com.dev_Group.smart_rdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRdvApplication.class, args);
+	}
+
+}
---
+++ Backend/smart_rdv/smart_rdv/src/test/java/com/dev_Group/smart_rdv/SmartRdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_Group.smart_rdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
